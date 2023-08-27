44d67febedece3208d838e72291dec39c86423b3
Gray Egboluche <69684799+G-r-ay@users.noreply.github.com>
2023-08-27 17:34:05 +0100

    Add files via upload

--- visuals.py
+++ visuals.py
@@ -34,25 +34,31 @@ st.sidebar.markdown(
     """,
     unsafe_allow_html=True
 )
-round_id = '0x2871742B184633f8DC8546c6301cbC209945033e'
+round_id = '0xb6Be0eCAfDb66DD848B0480db40056Ff94A9465d'
 if st.sidebar.button(
     "Climate Round", use_container_width=True
 ):
     round_id = '0xb6Be0eCAfDb66DD848B0480db40056Ff94A9465d'
     st.write("Climate Round")
-
+else:
+    st.write("Default: Climate Round")
 
 if st.sidebar.button(
     "Web3 Open Source Software", use_container_width=True
 ):
     round_id = '0x8de918F0163b2021839A8D84954dD7E8e151326D'
     st.write("Web3 Open Source Software")
+else:
+    st.write("Default: Climate Round")
 
 if st.sidebar.button(
     "Web3 Community and Education", use_container_width=True
 ):
     round_id = '0x2871742B184633f8DC8546c6301cbC209945033e'
     st.write("Web3 Community and Education")
+else:
+    st.write("Default: Climate Round")
+
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 @st.cache_data
