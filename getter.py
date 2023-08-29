97a48c0d6c9511518135983a3ca2941f9c4a40fb
Gray Egboluche <69684799+G-r-ay@users.noreply.github.com>
2023-08-29 15:24:45 +0100

    Add files via upload

--- getter.py
+++ getter.py
@@ -6,14 +6,12 @@ import streamlit as st
 
 @st.cache_data
 def get_existing_user_round_data(round_id):
-    print('call_exist')
     return pd.read_parquet(f'https://raw.githubusercontent.com/G-r-ay/G-SSD/main/archives/{round_id}.parquet')
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 @st.cache_data
 def get_sybil_addresses(round_id):
-    print('call_s_l')
     json_content = requests.get(f"https://raw.githubusercontent.com/G-r-ay/G-SSD/main/archives/{round_id}_sybil_cluster.json").json()
     unique_values = list(set(value for values_list in json_content.values() for value in values_list))
     return unique_values
@@ -22,7 +20,6 @@ def get_sybil_addresses(round_id):
 
 @st.cache_data
 def get_existing_time_data(round_id):
-    print('call_exist_time')
     return pd.read_parquet(f'https://raw.githubusercontent.com/G-r-ay/G-SSD/main/archives/{round_id}_time.parquet')
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -31,7 +28,6 @@ def get_existing_time_data(round_id):
 def get_date_existing(round_id,sybil_addresses):
     raw = getData(round_id,True)
     existing_data = get_existing_time_data(round_id)
-    print('data')
     time_data = pd.merge(raw,existing_data,on='transaction')
     time_data = label_dataframe(time_data,sybil_addresses)
     return time_data
@@ -41,11 +37,8 @@ def get_date_existing(round_id,sybil_addresses):
 @st.cache_data
 def get_labelled_existing(round_id):
     raw = getData(round_id,True)
-    print('data')
     sybil_addresses = get_sybil_addresses(round_id)
-    print('data')
     labelled_data = label_dataframe(raw, sybil_addresses)
-    print('data')
     return labelled_data,sybil_addresses
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- upkeep.py
+++ upkeep.py
@@ -6,27 +6,22 @@ from similarity_code import address_similarity
 
 #---------------------------------------------------------------------------------------------------------------
 def performUpKeep(updates,processed_data,round_id,existing_data):
-    print('starting')
     compile_data(updates,round_id,existing_data)
     fit_data = merger(existing_data,processed_data)
     s_address = address_similarity(fit_data,round_id)
     return s_address
 #---------------------------------------------------------------------------------------------------------------
 def checkUpKeep(round_id):
-    print('starter')
     raw = getData(round_id)
-    print('data')
     processed_data = preprocess_data(raw)
-    print('data')
     existing_data = get_existing_user_round_data(round_id)
     updates = list(set(processed_data['voter'].unique()) - set(existing_data['voter'].unique()))
+
     if len(updates) > 5:
-        print('data1')
         sybil_addresses = performUpKeep(updates,processed_data,round_id,existing_data)
         labelled_data = label_dataframe(raw,sybil_addresses)
         return labelled_data,sybil_addresses
     else:
-        print('upkeepnotneeded')
         sybil_addresses = get_sybil_addresses(round_id)
         labelled_data = label_dataframe(raw,sybil_addresses)
         return labelled_data,sybil_addresses
@@ -38,7 +33,6 @@ def checkUpKeep(round_id):
 def date_up_keep_update(round_id,sybil_addresses):
     raw = getData(round_id,True)
     existing_data = get_existing_time_data(round_id)
-    print('starting')
     updates = list(set(raw['transaction'].unique())-set(existing_data['transaction'].unique()))
     get_dates(updates,round_id)
     existing_data = pd.read_parquet(f'https://raw.githubusercontent.com/G-r-ay/G-SSD/main/archives/{round_id}_time.parquet')
